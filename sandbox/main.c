cac093f89132d1ba1e481a13cd5a548660ab959d
Jacob Marsh <jake.marsh@live.com.au>
2025-12-11 19:42:30 +0800

    Variables

--- sandbox/main.c
+++ sandbox/main.c
@@ -7,8 +7,28 @@ int main() {
     Test comment 3!
     */
 
-    printf("Wello \n");
-    printf("      Hurld!\n");
+    int age = 88; 
+    int year = 2025;
+    int quantity = 5;
+    
+    printf("You are %d years old\n", age);
+    printf("You are %d years old in the year %d\n", age, year);
+    printf("You ordered %d items\n", quantity - 1);
+
+    float gpa = 5;
+    
+    printf("Your GPA is %.2f\n", gpa);
+
+    float price = 19.99;
+    printf("The price is $%.0f\n", price);
+
+    float temperature = 25.7;
+    printf("The temperature is %.1f degrees\n", temperature);
+
+    double pi = 3.14159;
+    double e = 2.71828;
+    printf("The value of pi is %.10lf\n", pi);
+    printf("The value of e is %.10lf\n", e);
 
     return 0;
 }
