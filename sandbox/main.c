ab7253d1215aaa17ba3e61832e2df7fabf124897
Jacob Marsh <jake.marsh@live.com.au>
2025-12-11 21:48:12 +0800

    Format specifiers

--- sandbox/main.c
+++ sandbox/main.c
@@ -3,43 +3,25 @@
 
 int main() {
 
-    char grade = 'A';
-    char symbol = '#';
-    char currency[] = "AUD";
-    char email[] = "test@example.com";
+    int age = 25;
+    float price = 19.99;
+    double pi = 3.1415926535;
+    char currency = '$';
+    char name[] = "Jako";
 
-    printf("Your grade is %c\n", grade);
-    printf("Your fav symbol is %c\n", symbol);
-    printf("The currency is %s\n", currency);
-    printf("Your email is %s\n", email);
+    printf("%d\n", age);
+    printf("%f\n", price);
+    printf("%lf\n", pi);
+    printf("%c\n", currency);
+    printf("%s\n", name);
 
-    // Bools
+    float price1 = 19.99;
+    float price2 = 1.50;
+    float price3 = -100.00;
 
-    bool isOnline = true;
+    printf("%+7.1f\n", price1);
+    printf("%.2f\n", price2);
+    printf("%.3f\n", price3);
 
-    printf("Are you online? %d\n", isOnline);
-
-    if(isOnline) {
-        printf("You are online\n");
-    } else {
-        printf("You are offline\n");
-    }
-
-    bool isForSale = 0;
-    if(isForSale) {
-        printf("This item is for sale\n");
-    } else {
-        printf("This item is not for sale\n");
-    }
-
-    // variable = A reusable container for a value.
-    //      Behaves as if it were the value it contains.
-    // int = whole numbers (4 bytes in modern systems)
-    // float = single-precision decimal number (4 bytes)
-    // double = double-precision decimal number (8 bytes)
-    // char = single character (1 byte)
-    // char[] = array of characters (size varies)
-    // bool = true or false (1 byte, requires fstdbool.h>)
-    
     return 0;
 }
